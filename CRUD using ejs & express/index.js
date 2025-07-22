a697b9a0007a3bd89414922151193aa34d1239a6
Daxrokz10 <dakshgagnani@gmail.com>
2025-07-22 16:51:15 +0530

    crud

--- CRUD using ejs & express/index.js
+++ CRUD using ejs & express/index.js
@@ -1,27 +1,42 @@
 let users = [];
 
-
 const express = require('express');
-
 const app = express();
 const PORT = 9696;
 
-app.set('view engine','ejs')
-app.use(express.urlencoded({extended:true}));
-app.get('/',(req,res)=>{
-    return res.render('index.ejs', { users });
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { users, userToEdit: null });
 });
 
-app.post('/signup',(req,res)=>{
-    let data = {
-        email : req.body.email,
-        password : req.body.password
-    };
-    users.push(data);
-    return res.redirect('/');
-})
+app.post('/', (req, res) => {
+    users.push({ ...req.body, id: Date.now() });
+    res.redirect('/');
+});
 
-app.listen(PORT,()=>{
-    console.log('Server running on http://localhost:',PORT);
-    
-})
+app.get('/user/delete/:id', (req, res) => {
+    const id = Number(req.params.id);
+    users = users.filter(user => user.id !== id);
+    res.redirect('/');
+});
+
+app.get('/user/edit/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const userToEdit = users.find(user => user.id === id);
+    if (!userToEdit) return res.redirect('/');
+    res.render('index.ejs', { users, userToEdit });
+});
+
+app.post('/user/edit/:id', (req, res) => {
+    const id = Number(req.params.id);
+    users = users.map(user =>
+        user.id === id ? { ...req.body, id } : user
+    );
+    res.redirect('/');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
