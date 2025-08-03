0effe9302f45df4b0e10002cd12487748b6a0086
Daxrokz10 <dakshgagnani@gmail.com>
2025-08-03 18:43:53 +0530

    login system

--- Practice/index.js
+++ Practice/index.js
@@ -10,20 +10,42 @@ const Employee = require('./models/practice')
 
 app.get('/', async (req,res)=>{
     const all = await Employee.find();
+    res.render('login',{employees: all});
+});
+app.post('/',async(req,res)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+
+    Employee.findOne({email: email, password: password})
+        .then((data) => {
+            if (data) {
+                res.redirect('/home');
+            } else {
+                res.render('login', { message: 'Invalid email or password' });
+            }
+        })
+        .catch((err) => {
+            console.error('Error during login:', err);
+            res.status(500).send('Internal Server Error');
+        });
+})
+
+app.get('/home', async (req,res)=>{
+    const all = await Employee.find();
     res.render('index',{employees: all});
 });
 
 app.post('/add',async(req,res)=>{
     const data = new Employee(req.body);
     await data.save();
-    res.redirect('/');
+    res.redirect('/home');
 });
 
 app.post('/employee/delete/:id',async (req,res)=>{
     const {id} = req.params;
     console.log(id);
     await Employee.findByIdAndDelete(id);
-    return res.redirect('/');
+    return res.redirect('/home');
 })
 app.post('/employee/edit/:id',async (req,res)=>{
     const {id} = req.params;
@@ -35,7 +57,7 @@ app.post('/editedData/:id', (req, res) => {
     const { email, password } = req.body;
 
     Employee.findByIdAndUpdate(id, { email, password }, { new: true })
-        .then(() => res.redirect('/'))
+        .then(() => res.redirect('/home'))
         .catch((err) => {
             console.error('Error updating employee:', err);
             res.status(500).send('Error updating employee');
