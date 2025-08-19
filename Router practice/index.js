c12ad6fe0cba0afa43abcc94ba7021927eed8f55
Daxrokz10 <dakshgagnani@gmail.com>
2025-08-19 16:55:35 +0530

    commit

--- Router practice/index.js
+++ Router practice/index.js
@@ -10,7 +10,6 @@ const db = require('./config/db');
 const Product = require('./models/productSchema');
 
 app.use('/',require('./routers'))
-app.use('/products',require('./routers'))
 
 app.listen(port,(err)=>{
     if(!err){
